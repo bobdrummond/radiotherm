e4e4c2e864af4af5741dfa1cbc256fda38f05a34
Bob Drummond <bob.drummond@gmail.com>
2016-06-16 01:29:17 -0400

    fix error output in run-tests.sh when python is not 2.6

--- run-tests.sh
+++ run-tests.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-is_py26=`python -c 'import sys; print sys.version_info < (2, 7)'`
+is_py26=`python -c 'import sys; print sys.version_info < (2, 7)' 2>&1`
 
-if [ $is_py26 = "True" ]; then
+if [ "$is_py26" = "True" ]; then
     unit2 discover
 else
     python -m unittest discover
